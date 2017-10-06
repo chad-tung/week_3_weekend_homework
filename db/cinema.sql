dc38324d035bcbdc2e8d15076a6bb4ac37f09cfc
chad-tung <chad.tung@gmail.com>
2017-10-06 15:43:35 +0100

    dropped and created tables, typed out Sql runner file.

--- db/cinema.sql
+++ db/cinema.sql
@@ -16,10 +16,9 @@ CREATE TABLE customers (
 CREATE TABLE films (
     id SERIAL8 PRIMARY KEY,
     title VARCHAR(255),
-    type VARCHAR(255), -- Will be like 3D, but since only one screen, maybe not...
-    base_price SMALLINT,
+    price SMALLINT,
     age_rating SMALLINT,
-    duration TIME
+    duration INT
 );
 
 CREATE TABLE screenings (
@@ -27,7 +26,6 @@ CREATE TABLE screenings (
     film_id INT8 REFERENCES films(id),
     seatings SMALLINT,
     start_time TIME,
-    end_time TIME,
     day VARCHAR(255)
 );
 
@@ -35,4 +33,11 @@ CREATE TABLE tickets (
     id SERIAL8 PRIMARY KEY,
     screening_id INT8 REFERENCES screenings(id),
     customer_id INT8 REFERENCES customers(id)
+);
+
+CREATE TABLE items(
+    id SERIAL8 PRIMARY KEY,
+    name VARCHAR(255),
+    price SMALLINT,
+    stock SMALLINT
 );
